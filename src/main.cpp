7fd3a87afed7479095bea6e26e62e7c83aae5ff6
Stavros Mantzouneas <930338+mantzoun@users.noreply.github.com>
2024-11-11 16:30:54 +0200

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,60 @@
+/*
+ * main.cpp
+ */
+
+#include <stdio.h>
+#include <unistd.h>
+#include <iostream>
+#include <map>
+#include <mosquitto.h>
+#include <cstring>
+
+#include "discord_bot.h"
+
+
+static luxbracer::DiscordBot bot;
+
+int main(int argc, char** argv)
+{
+//    if (argc < 2){
+//        std::cout << "Usage: cdb <config_file>\n";
+//        exit(1);
+//    }
+
+//    std::string config_file = argv[1];
+
+//    cdb::Logger logger = cdb::Logger(cdb::CDB_LOG_DEBUG);
+
+//    cdb::IO io;
+//    cdb::Configurator conf;
+//    cdb::MqttHandler m_handler;
+
+//    conf.set_logger(&logger);
+//    conf.config_read(config_file);
+//    logger.set_level(conf.get_log_level());
+
+//    m_handler.set_logger(&logger);
+//    io.set_logger(&logger);
+
+//    logger.info("Reading config file");
+
+//    io.fifo_init("/tmp/mqtt_disc_cpp.fifo");
+
+//    logger.info("Starting Discord Bot\n");
+//    bot.set_logger(&logger);
+    std::string token = "";
+    std::string id = "myid";
+    bot.init(token, id);
+//    bot.set_mqtt_handler(&m_handler);
+
+//    m_handler.set_discord_bot(&bot);
+//    m_handler.init(&conf);
+
+//    io.set_discord_bot(&bot);
+
+//    logger.info("Init done, entering loop");
+
+    while(1){
+        usleep(1000000);
+    }
+}
