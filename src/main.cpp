fdb3b4b7436690681789d6004a80ed98afe442c1
Stavros Mantzouneas <930338+mantzoun@users.noreply.github.com>
2025-05-29 22:59:51 +0300

    fix history editting shenanigans

--- src/main.cpp
+++ src/main.cpp
@@ -28,43 +28,6 @@ namespace luxbracer {
     int main(int argc, char** argv) {
         logger.info("Starting Discord Bot\n");
 
-        Planet p("p1", "s1", 0);
-        System s("s1", 0);
-
-//    cdb::Logger logger = cdb::Logger(cdb::CDB_LOG_DEBUG);
-
-//    cdb::IO io;
-//    cdb::Configurator conf;
-//    cdb::MqttHandler m_handler;
-
-//    conf.set_logger(&logger);
-//    conf.config_read(config_file);
-//    logger.set_level(conf.get_log_level());
-
-//    m_handler.set_logger(&logger);
-//    io.set_logger(&logger);
-
-//    logger.info("Reading config file");
-
-//    io.fifo_init("/tmp/mqtt_disc_cpp.fifo");
-
-//    logger.info("Starting Discord Bot\n");
-//    bot.set_logger(&logger);
-    std::string token = "";
-    std::string id = "myid";
-    bot.init(token, id);
-
-//    bot.set_mqtt_handler(&m_handler);
-
-//    m_handler.set_discord_bot(&bot);
-//    m_handler.init(&conf);
-
-//    io.set_discord_bot(&bot);
-
-//    logger.info("Init done, entering loop");
-
-    while(1){
-        usleep(1000000);
         engine.setLogger(&logger);
         engine.set_postman(&bot);
 #ifdef ENGINE_INIT_FROM_DISCORD
@@ -74,7 +37,7 @@ namespace luxbracer {
 
         bot.set_logger(&logger);
         bot.set_engine(&engine);
-        std::string token = ""
+        std::string token = "";
         std::string id = "myid";
         bot.init(token, id);
 
